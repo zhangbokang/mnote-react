56857f0de8f83c76ec7998d5b5e27e17c7840aa7
zhangbokang <540064991@qq.com>
2018-12-05 22:55:29 +0800

    删除不用的文件

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,9 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
-
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+class Main extends React.Component {
 
-class AppComponent extends React.Component {
   render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
+    return <div>main</div>
   }
+
 }
-
-AppComponent.defaultProps = {
-};
-
-export default AppComponent;
